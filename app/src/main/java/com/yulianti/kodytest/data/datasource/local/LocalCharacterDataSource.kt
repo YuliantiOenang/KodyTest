e70b45c5bfd520491f90c3477d0d633883f26396
YuliantiOenang <yuliantioenang@gmail.com>
2024-12-02 12:52:27 +0700

    use withContext in DataSource

--- app/src/main/java/com/yulianti/kodytest/data/datasource/local/LocalCharacterDataSource.kt
+++ app/src/main/java/com/yulianti/kodytest/data/datasource/local/LocalCharacterDataSource.kt
@@ -7,6 +7,8 @@ import com.yulianti.kodytest.data.model.Character
 import com.yulianti.kodytest.data.model.CustomResult
 import com.yulianti.kodytest.data.model.DataError
 import com.yulianti.kodytest.data.model.PaginatedResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LocalCharacterDataSource @Inject constructor(
@@ -16,23 +18,25 @@ class LocalCharacterDataSource @Inject constructor(
         name: String?
     ): CustomResult<List<Character>, DataError> {
         return try {
-            val result = if (name?.isNotEmpty() == true) {
-                characterDao.getCharacterByQuery(name).map {
-                    Character(
-                        it.id,
-                        it.name,
-                        it.coverUrl,
-                        it.description
-                    )
-                }
-            } else {
-                characterDao.getAllCharacters().map {
-                    Character(
-                        it.id,
-                        it.name,
-                        it.coverUrl,
-                        it.description
-                    )
+            val result = withContext(Dispatchers.IO) {
+                if (name?.isNotEmpty() == true) {
+                    characterDao.getCharacterByQuery(name).map {
+                        Character(
+                            it.id,
+                            it.name,
+                            it.coverUrl,
+                            it.description
+                        )
+                    }
+                } else {
+                    characterDao.getAllCharacters().map {
+                        Character(
+                            it.id,
+                            it.name,
+                            it.coverUrl,
+                            it.description
+                        )
+                    }
                 }
             }
             CustomResult.Success(result)
@@ -43,14 +47,21 @@ class LocalCharacterDataSource @Inject constructor(
 
     suspend fun saveCharacter(
         characters: PaginatedResult<Character>
-    ): CustomResult<List<Character>, DataError> {
+    ): CustomResult<Unit, DataError> { // Use Unit if no meaningful response is needed
         return try {
-            characterDao.insertAllCharacter(characters.items.map { CharacterEntity(it.id, it.name, it.coverUrl, it.description) })
-            CustomResult.Success(listOf())
-        }  catch (e: SQLiteFullException) {
+            withContext(Dispatchers.IO) {
+                characterDao.insertAllCharacter(
+                    characters.items.map {
+                        CharacterEntity(it.id, it.name, it.coverUrl, it.description)
+                    }
+                )
+            }
+            CustomResult.Success(Unit)  // Indicate success with Unit
+        } catch (e: SQLiteFullException) {
             CustomResult.Error(DataError.Local.DISK_FULL)
         } catch (e: Exception) {
             CustomResult.Error(DataError.Local.UNKNOWN)
         }
     }
+
 }
--- app/src/main/java/com/yulianti/kodytest/data/datasource/network/NetworkCharacterDataSource.kt
+++ app/src/main/java/com/yulianti/kodytest/data/datasource/network/NetworkCharacterDataSource.kt
@@ -7,6 +7,8 @@ import com.yulianti.kodytest.data.model.DataError
 import com.yulianti.kodytest.data.datasource.network.service.CharacterService
 import com.yulianti.kodytest.data.model.PaginatedResult
 import com.yulianti.kodytest.util.MD5Util
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import javax.inject.Inject
 
@@ -19,31 +21,33 @@ class NetworkCharacterDataSource @Inject constructor(private val service: Charac
     ): CustomResult<PaginatedResult<Character>, DataError> {
         val timestamp = System.currentTimeMillis().toString()
         return try {
-            val allResult = service.getCharacter(
-                toMap(
-                    name,
-                    timestamp,
-                    BuildConfig.PUBLIC_KEY,
-                    MD5Util.md5(timestamp + BuildConfig.PRIVATE_KEY + BuildConfig.PUBLIC_KEY),
-                    limit, offset
+            withContext(Dispatchers.IO) {
+                val allResult = service.getCharacter(
+                    toMap(
+                        name,
+                        timestamp,
+                        BuildConfig.PUBLIC_KEY,
+                        MD5Util.md5(timestamp + BuildConfig.PRIVATE_KEY + BuildConfig.PUBLIC_KEY),
+                        limit, offset
+                    )
                 )
-            )
-            val result = allResult.data.results.map {
-                Character(
-                    it.id,
-                    it.name,
-                    it.thumbnail.path + "." + it.thumbnail.extension,
-                    it.description
+                val result = allResult.data.results.map {
+                    Character(
+                        it.id,
+                        it.name,
+                        it.thumbnail.path + "." + it.thumbnail.extension,
+                        it.description
+                    )
+                }
+                CustomResult.Success(
+                    PaginatedResult(
+                        result,
+                        allResult.data.total,
+                        allResult.data.offset,
+                        allResult.data.count
+                    )
                 )
             }
-            CustomResult.Success(
-                PaginatedResult(
-                    result,
-                    allResult.data.total,
-                    allResult.data.offset,
-                    allResult.data.count
-                )
-            )
         } catch (e: HttpException) {
             when (e.code()) {
                 408 -> CustomResult.Error(DataError.Network.REQUEST_TIMEOUT)
@@ -86,19 +90,21 @@ class NetworkCharacterDataSource @Inject constructor(private val service: Charac
     override suspend fun fetchCharacterDetail(characterId: Int): CustomResult<Character, DataError> {
         val timestamp = System.currentTimeMillis().toString()
         return try {
-            val result = service.getCharacterDetail(
-                characterId,
-                timestamp,
-                BuildConfig.PUBLIC_KEY,
-                MD5Util.md5(timestamp + BuildConfig.PRIVATE_KEY + BuildConfig.PUBLIC_KEY)
-            ).data.results.first()
-            val newResult = Character(
-                result.id,
-                result.name,
-                result.thumbnail.path + "." + result.thumbnail.extension,
-                result.description
-            )
-            CustomResult.Success(newResult)
+            withContext(Dispatchers.IO) {
+                val result = service.getCharacterDetail(
+                    characterId,
+                    timestamp,
+                    BuildConfig.PUBLIC_KEY,
+                    MD5Util.md5(timestamp + BuildConfig.PRIVATE_KEY + BuildConfig.PUBLIC_KEY)
+                ).data.results.first()
+                val newResult = Character(
+                    result.id,
+                    result.name,
+                    result.thumbnail.path + "." + result.thumbnail.extension,
+                    result.description
+                )
+                CustomResult.Success(newResult)
+            }
         } catch (e: HttpException) {
             when (e.code()) {
                 408 -> CustomResult.Error(DataError.Network.REQUEST_TIMEOUT)
